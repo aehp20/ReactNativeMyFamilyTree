112a3f5198fc8d6887e394faa45b6d46b824014d
Aron HERRERA PONTE <aronherrera2009@gmail.com>
2024-08-04 17:07:46 +0200

    chore: update prettier and use @eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  {
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    languageOptions: {
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
