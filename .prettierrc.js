3b72373c97360589409bf88c2283fb46c4910794
Aron HERRERA PONTE <aronherrera2009@gmail.com>
2024-08-06 00:56:56 +0200

    chore: update prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,14 @@
 module.exports = {
-  arrowParens: 'avoid',
+  semi: true,
+  arrowParens: 'always',
   bracketSameLine: true,
   bracketSpacing: true,
   singleQuote: true,
   trailingComma: 'all',
   printWidth: 80,
   tabWidth: 2,
+  useTabs: false,
+  jsxSingleQuote: false,
+  jsxBracketSameLine: false,
+  endOfLine: 'lf',
 };
