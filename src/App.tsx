86e55481c9532144f5f8e4e49545e6b388b65d1e
Aron HERRERA PONTE <aronherrera2009@gmail.com>
2024-08-06 18:18:15 +0200

    add section for test purpose

--- src/App.tsx
+++ src/App.tsx
@@ -92,6 +92,9 @@ function App(): React.JSX.Element {
           <Section title="Learn More">
             Read the docs to discover what to do next:
           </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
           <LearnMoreLinks />
         </View>
       </ScrollView>
